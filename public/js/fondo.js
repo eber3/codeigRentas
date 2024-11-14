f94e556944a7f078c69b1484d197096915917e27
Your Name <you@example.com>
2024-11-14 08:47:27 -0600

    borre

--- public/js/fondo.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('scroll', function() {
-    let offset = window.pageYOffset;
-    document.querySelector('.header').style.backgroundPosition = `center ${offset * 0.5}px`;
-});
